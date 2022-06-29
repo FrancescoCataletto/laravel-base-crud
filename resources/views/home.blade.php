3c24dc302b98d93cc7b85bbde7adc05d9a8adf9d
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-06-29 17:37:18 +0200

    printed comics in page

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,6 +1,16 @@
 @extends('layouts.main')
 
 @section('content')
-<h1>PROVA 2</h1>
-@dd($comics);
+<h1>COMICS</h1>
+
+@foreach ($comics as $comic)
+    <div>
+        <ul>
+            <li>{{$comic->title}}</li>
+            <li><img src="{{$comic->image}}" alt="{{$comic->title}}"></li>
+            <li>{{$comic->type}}</li>
+        </ul>
+    </div>
+@endforeach
+
 @endsection
