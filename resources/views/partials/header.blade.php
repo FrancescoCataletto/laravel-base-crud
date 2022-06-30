018ae5912fc7e1fbdae9b051f28e2165830e3424
FrancescoCataletto <catalettofrancesco@gmail.com>
2022-06-30 16:18:19 +0200

    styled header

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,11 @@
+<header>
+
+    <div>
+        <ul>
+            <li><a href="#">HOME</a></li>
+            <li><a href="#">I FUMETTI</a></li>
+            <li><a href="#">AGGIUNGI FUMETTO</a></li>
+        </ul>
+    </div>
+
+</header>
